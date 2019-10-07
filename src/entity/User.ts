d69941adae212b1fb7e015175fd9d3b87de8e06d
Chau Dinh <chaudvb95uc@gmail.com>
2019-10-07 16:12:42 +0700

    register resolver

---
+++ src/entity/User.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { ObjectType, Field, ID } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User extends BaseEntity {
+  // Use Column decorator to store in the database
+  @Field(() => ID) // I use Field() decorator to expose this to graphql. It means that we set id, firstName, lastName and email are graphql field but password is a database field
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  firstName: string;
+
+  @Field()
+  @Column()
+  lastName: string;
+
+  @Field()
+  @Column("text", { unique: true })
+  email: string;
+
+  @Field()
+  name: string;
+
+  @Column()
+  password: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import * as Express from "express";
-import { buildSchema, Resolver, Query } from "type-graphql";
-
-@Resolver()
-class HelloResolver {
-  @Query(() => String, { name: "helloWorld" }) // we can change the name of the query here
-  async hello() {
-    // the function name is also the name of the query by default
-    return "hello, world";
-  }
-}
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { RegisterResolver } from "./modules/user/Register";
 
 const main = async () => {
+  // Create connection
+  await createConnection(); // will read from the orm config and use these setting to make database connection
+
   const schema = await buildSchema({
-    resolvers: [HelloResolver]
+    resolvers: [RegisterResolver]
   });
   const apolloServer = new ApolloServer({ schema });
 
