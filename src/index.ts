9c5e2c9c9319320e78bd06f35cce321d8b689822
Chau Dinh <chaudvb95uc@gmail.com>
2019-10-08 14:23:18 +0700

    login resolver

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,53 @@ import { ApolloServer } from "apollo-server-express";
 import Express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
+import { RedisClient } from "redis";
+
 import { RegisterResolver } from "./modules/user/Register";
+import { redis } from "./redis";
+import { LoginResolver } from "./modules/user/Login";
+import { MeResolver } from "./modules/user/Me";
 
 const main = async () => {
   // Create connection
   await createConnection(); // will read from the orm config and use these setting to make database connection
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver]
+    resolvers: [MeResolver, RegisterResolver, LoginResolver]
   });
   const apolloServer = new ApolloServer({
-    schema
+    schema,
+    context: ({ req }: any) => ({ req })
   });
 
   const app = Express();
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000"
+    })
+  );
+
+  // Session middleware
+  const RedisStore = connectRedis(session); // connect node.req.session to redis backing store
+  const sessionOption: session.SessionOptions = {
+    store: new RedisStore({
+      client: (redis as any) as RedisClient
+    }),
+    name: "this",
+    secret: "sdfgh34567",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24 * 7 * 365 // 7 years
+    }
+  };
+  app.use(session(sessionOption));
 
   apolloServer.applyMiddleware({ app });
 
---
+++ src/modules/user/Login.ts
@@ -0,0 +1,30 @@
+import { Resolver, Mutation, Arg, Ctx } from "type-graphql";
+import bcrypt from "bcryptjs";
+import { User } from "../../entity/User";
+import { MyContext } from "src/types/MyContext";
+
+@Resolver()
+export class LoginResolver {
+  @Mutation(() => User, { nullable: true })
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() ctx: MyContext
+  ): Promise<User | null> {
+    // Check email
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return null;
+    }
+
+    // Compare to password
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      return null;
+    }
+
+    ctx.req.session!.userId = user.id;
+
+    return user;
+  }
+}
