0c7c8c5a91945d2f0980a1d59aa98c6824f2ae42
Chau Dinh <chaudvb95uc@gmail.com>
2019-10-09 16:34:23 +0700

    logout

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const main = async () => {
   });
   const apolloServer = new ApolloServer({
     schema,
-    context: ({ req }: any) => ({ req })
+    context: ({ req, res }: any) => ({ req, res })
   });
 
   const app = Express();
